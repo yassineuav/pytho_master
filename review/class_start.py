b0735c6eb178dd2501b49bcf1aa55fee9973336e
Yassine Khaidouch <91646673+yassineuav@users.noreply.github.com>
2023-07-28 00:44:13 +0000

    class

--- review/class_start.py
+++ review/class_start.py
@@ -1,7 +1,13 @@
 
 
 class Person:
+    "this is a person class"
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        
+
+    def print_name(self):
+        print(self.name)
+
+
+Person("yassine", 28).print_name()
