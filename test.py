b6f90cf4ce202751a5621fb1b27040b824e3c0ae
Yassine Khaidouch <91646673+yassineuav@users.noreply.github.com>
2023-09-09 00:38:18 -0700

    class

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
-class doubes:
-    def __init__(self, times, amount):
-        return 0
+class test:
+    def __init__(self, amount, times=10):
+        self.amount = amount
+        self.times = times
     
+        for i in range(0, times):
+            balance = "{:,}".format(self.amount)
+            print(f"{i}  ${balance}")
+            self.amount = self.amount * 2
+            
+
+double = test(100, 11)
