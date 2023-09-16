34e5f09e05079b57fc3bcc7091e810afbe5bfb45
Yassine Khaidouch <91646673+yassineuav@users.noreply.github.com>
2023-09-16 12:19:40 +0000

    calendar

--- test.py
+++ test.py
@@ -1,12 +1,31 @@
 class test:
-    def __init__(self, amount, times=10):
+    def __init__(self, amount=100, times=10, percent=1.5):
         self.amount = amount
         self.times = times
+        self.percent = percent
     
         for i in range(0, times):
-            balance = "{:,}".format(self.amount)
-            print(f"{i}  ${balance}")
-            self.amount = self.amount * 2
+            # balance = "{:,}".format(self.amount)
+            print(f"{i}  ${self.amount:,}")
+            self.amount = self.amount * percent
             
 
-double = test(100, 11)
+# double = test(100, 11, 2)
+
+class list_weekday:
+    day_names = [         
+        'Monday',         
+        'Tuesday',         
+        'Wednesday',         
+        'Thursday',         
+        'Friday',         
+        'Saturday',
+        'Sunday',    
+    ]
+    # import calendar  
+    # day_names = list(calendar.day_name)
+    print(day_names)
+
+list_weekday
+
+
