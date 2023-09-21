a12c479f17499f0394194eb9ffd9c91050620b02
Yassine Khaidouch <91646673+yassineuav@users.noreply.github.com>
2023-09-21 00:35:10 +0000

    test

--- test.py
+++ test.py
@@ -8,13 +8,15 @@ class test:
         
         self.percentage = 100
         self.percentage_2 = 100
-        print(f"times  balance 100%   50%")
+        print(f"times  balance 100%")
         for i in range(0, times):
             # balance = "{:,}".format(self.amount)
-            print(f"{i}   ${self.amount_full:,}  ${self.amount:,}  {self.percentage}%  {self.percentage_2}% ")
+            print(f"{i}   ${self.amount_full:,}  ${self.amount:,}  {self.percentage}% ")
             self.amount = self.amount * 1.5
             self.amount_full = self.amount_full * 2
             self.percentage *= 2
             self.percentage_2 *= 1.5
-
+    def __str__(self):
+        return f"{self.amount=}"    
+    
 double = test(amount=100, times=11, percent=2)
