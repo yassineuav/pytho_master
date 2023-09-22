546c87099d4d0e6ba6da0ca959aebc7b7ef31f03
Yassine Khaidouch <91646673+yassineuav@users.noreply.github.com>
2023-09-22 12:37:42 +0000

    week count

--- test.py
+++ test.py
@@ -1,22 +1,23 @@
 class test:
     
-    def __init__(self, amount=100, times=10, percent=1.5):
+    def __init__(self, amount=100, times=11, percent=1.5):
         self.amount = amount
-        self.amount_full = amount
         self.times = times
         self.percent = percent
-        
+        self.week = 1
         self.percentage = 100
-        self.percentage_2 = 100
         print(f"times  balance 100%")
+        self.week  = 1
         for i in range(0, times):
             # balance = "{:,}".format(self.amount)
-            print(f"{i}   ${self.amount_full:,}  ${self.amount:,}  {self.percentage}% ")
-            self.amount = self.amount * 1.5
-            self.amount_full = self.amount_full * 2
+            if i in [1, 6, 11, 16]:
+                print(f"week {self.week}")
+                self.week += 1
+            print(f"{i}    ${self.amount:,}  {self.percentage}% ")
+            self.amount = self.amount * 2
             self.percentage *= 2
-            self.percentage_2 *= 1.5
+            
     def __str__(self):
         return f"{self.amount=}"    
     
-double = test(amount=100, times=11, percent=2)
+double = test(amount=100, times=20, percent=2)
