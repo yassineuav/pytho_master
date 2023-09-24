2c48292557ab3202ec5a72121b14053846833004
Yassine Khaidouch <91646673+yassineuav@users.noreply.github.com>
2023-09-23 22:30:00 -0700

    Create mysql_crud.py

---
+++ review/mysql_crud.py
@@ -0,0 +1,64 @@
+import mysql.connector
+
+# Connect to MySQL
+conn = mysql.connector.connect(
+    host='your_mysql_host',
+    user='your_username',
+    password='your_password',
+    database='your_database'
+)
+
+# Create a cursor object to execute SQL queries
+cursor = conn.cursor()
+
+# Create a table
+create_table_query = '''
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT
+)
+'''
+cursor.execute(create_table_query)
+
+# Insert data
+insert_query = '''
+INSERT INTO users (name, age)
+VALUES (%s, %s)
+'''
+data = ('Alice', 30)
+cursor.execute(insert_query, data)
+
+# Commit the transaction
+conn.commit()
+
+# Read data
+select_query = '''
+SELECT * FROM users
+'''
+cursor.execute(select_query)
+for row in cursor.fetchall():
+    print(row)
+
+# Update data
+update_query = '''
+UPDATE users
+SET age = %s
+WHERE name = %s
+'''
+data = (35, 'Alice')
+cursor.execute(update_query, data)
+conn.commit()
+
+# Delete data
+delete_query = '''
+DELETE FROM users
+WHERE name = %s
+'''
+data = ('Alice',)
+cursor.execute(delete_query, data)
+conn.commit()
+
+# Close cursor and connection
+cursor.close()
+conn.close()
